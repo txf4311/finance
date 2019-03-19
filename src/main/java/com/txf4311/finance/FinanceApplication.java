cbbc21818c214d6160302e44475af0a7114f42bb
stonetang <txf4311@163.com>
2019-03-19 15:50:52 +0800

    finance project  for spring boot

---
+++ src/main/java/com/txf4311/finance/FinanceApplication.java
@@ -0,0 +1,16 @@
+package com.txf4311.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author txf4311
+ */
+@SpringBootApplication
+public class FinanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceApplication.class, args);
+    }
+
+}
